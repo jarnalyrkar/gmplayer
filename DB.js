c7d90c3249bfd780b0deca8322a7bfc595ef9a60
jarnalyrkar <joachimsolstrom@gmail.com>
2023-08-03 23:12:52 +0200

    Working db-class, more to come

--- DB.js
+++ DB.js
@@ -1,21 +1,54 @@
 class DB {
 
   constructor() {
-    const sqlite3 = require('sqlite3').verbose()
-    this.db = new sqlite3.Database('gmplayer.db')
+    this.db = require('better-sqlite3')('gmplayer.db')
+    this.db.pragma('journal_mode = WAL')
+
+    this.default_colors = {
+      'primary': "hsl(25, 56%, 25%)",
+      'accent': "hsl(43, 74%, 49%)",
+      'text': "hsl(0,0%,100%)"
+    }
   }
 
+  // TODO: Add all methods from db.php here
+
   get_setting_by_name(name) {
-    console.log(name)
-    return name
-    // const sql = "SELECT * FROM settings WHERE option = ?"
-    // let results = []
-    // db.run(sql, [name], (err, rows) => {
-    //   results.push(rows)
-    // })
+    const stmt = this.db.prepare(`SELECT value FROM settings WHERE option = ?`)
+    const setting = stmt.get(name)
+    return setting
   }
-  get_init_settings() {
-    return {'option': 'last_theme_id', 'value': 123}
+
+  get_primary_color() {
+    return this.get_setting_by_name('primary_color')
+  }
+
+  set_primary_color(color) {
+    // Update method
+  }
+
+  get_accent_color() {
+    return this.get_setting_by_name('accent_color')
+  }
+
+  set_accent_color(color) {
+    const stmt = this.db.prepare()
+  }
+
+  get_text_color() {
+    return this.get_setting_by_name('text_color')
+  }
+
+  set_text_color(color) {
+    const stmt = this.db.prepare(`UPDATE settings SET value = ? WHERE option = 'text_color'`)
+    const info = stmt.run(color)
+    return info.changes
+  }
+
+  reset_theme() {
+    // set_primary_color(this.default_colors.primary)
+    // set_accent_color(this.default_colors.accent)
+    // set_text_color(this.default_colors.text)
   }
 
 }
--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 const { app, BrowserWindow } = require('electron')
 const ejse = require('ejs-electron')
+const DB = require('./DB')
+
+const db = new DB()
+
+// TODO: expose db-functions to mainWorld
+// so we can call db-methods from client
+
 
 let mainWindow
 
@@ -7,6 +14,6 @@ ejse.data('last_active_theme', 1)
 ejse.data('last_active_preset', 2)
 
 app.on('ready', () => {
-  mainWindow = new BrowserWindow()
-  mainWindow.loadURL('file://' + __dirname + '/views/index.ejs')
+  // mainWindow = new BrowserWindow()
+  // mainWindow.loadURL('file://' + __dirname + '/views/index.ejs')
 })
