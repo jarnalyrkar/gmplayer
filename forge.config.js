13100373817c091bdbcb2bb54f504ab042628295
jarnalyrkar <joachimsolstrom@gmail.com>
2023-08-13 00:04:44 +0200

    Update makers

--- forge.config.js
+++ forge.config.js
@@ -8,8 +8,14 @@ module.exports = {
   makers: [
     {
       name: '@electron-forge/maker-deb',
-      config: {},
-    },
+    }, {
+      name: '@electron-forge/maker-wix',
+    }, {
+      name: '@electron-forge/maker-zip'
+    }, {
+      name: '@electron-forge/maker-dmg',
+    }
+
   ],
   plugins: [
     {
